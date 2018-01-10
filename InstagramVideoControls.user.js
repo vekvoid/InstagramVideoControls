33bfeeae61602f2805dd782d63a367ff9aebc37c
vekvoid <vektrm@gmail.com>
2018-01-10 17:15:27 +0100

    Apply only once per video.

--- InstagramVideoControls.user.js
+++ InstagramVideoControls.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Instagram Video Controls
 // @namespace    https://github.com/vekvoid/
-// @version      1.0
+// @version      0.2
 // @description  try to take over the world!
 // @author       Vekvoid
 // @match        *://www.instagram.com/*
@@ -12,7 +12,8 @@
 (function() {
   'use strict';
   $('body').on('DOMSubtreeModified', 'main > section > div > div:first-child', function() {
-    // Need to be optimized
-    $('video').attr({ controls: '', style: 'z-index: 3;' });
+    $('video:not(.vekv-videoControls)')
+      .attr({ controls: '', style: 'z-index: 1;' })
+      .addClass('vekv-videoControls');
   });
 })();
